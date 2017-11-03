f6b3797425db16d145df7379b415ce4aa629ca57
yasui321 <sample@sample.com>
2017-11-03 08:06:57 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :users, only:[:index, :show]
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'home#index'
 end
